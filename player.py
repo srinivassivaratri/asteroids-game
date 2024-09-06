52f9a9057aaa0dd5a3ef5fa1e176e0f8067da31e
srinivassivaratri <srinivassivaratri1122@gmail.com>
2024-09-05 21:58:17 -0500

    player able to move in upward direction with w key and with s key in downward direction.

--- player.py
+++ player.py
@@ -29,3 +29,11 @@ class Player(CircleShape):
             self.rotate(-dt)  # Invert dt for left rotation
         if keys[pygame.K_d]:
             self.rotate(dt)  # Right rotation
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
