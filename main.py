d96f30a4bb7661e267e87fb8415a5ce178e6b82f
srinivassivaratri <srinivassivaratri1122@gmail.com>
2024-09-05 21:46:44 -0500

    allowing a and d keys for the player to rotate left and right respectively

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 
 # Write a main function that simply prints "Starting asteroids!" (use this exact text).
@@ -10,16 +11,26 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # calling the update method before rendering.
+        player.update(dt)
+        
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-
+  
         #limiting framerate to 60 frames per second(fps)
         dt = clock.tick(60) / 1000
+    
+    
+
+    
     
    
 
