27646480484f71190afaee17b2173c619c2d2f62
srinivassivaratri <srinivassivaratri1122@gmail.com>
2024-09-06 17:53:10 -0500

    created asteroid and asteroidfield files

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 # Write a main function that simply prints "Starting asteroids!" (use this exact text).
@@ -14,11 +16,17 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
     Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0
+
 
     while True:
         for event in pygame.event.get():
