a835783a2b9a500d995e123e5eb02b4987aa5b4e
srinivassivaratri <srinivassivaratri1122@gmail.com>
2024-09-06 19:18:51 -0500

    limiting rate at which bullets are shot by 0.3 seconds

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
     
     # in the player class
     def triangle(self):
@@ -24,6 +25,7 @@ class Player(CircleShape):
         self.rotation += (PLAYER_TURN_SPEED * dt)
     
     def update(self, dt):
+        self.shoot_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -42,6 +44,11 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        if self.shoot_timer > 0:
+            return
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
         shot = Shot(self.position.x, self.position.y)
         forward = pygame.Vector2(0,1).rotate(self.rotation)
         shot.velocity = forward * PLAYER_SHOOT_SPEED
+
+    
