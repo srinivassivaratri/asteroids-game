9fe5265d885c23c898d395618f5ce3e5f2a4faa6
srinivassivaratri <srinivassivaratri1122@gmail.com>
2024-09-06 19:53:56 -0500

    splitting asteroids into 2 when shot logic in asteroid.py file

--- main.py
+++ main.py
@@ -47,6 +47,12 @@ def main():
                 print("Game over!")   
                 sys.exit()
 
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+                    
+
         screen.fill("black")
 
         for obj in drawable:
